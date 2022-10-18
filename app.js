12e82be4ad5aaed8b7965178de15ae6033c16d8c
Denisse Orellana <denissep.orellana@gmail.com>
2022-10-18 16:59:14 -0300

    change: main.css

--- app.js
+++ app.js
@@ -19,16 +19,28 @@ let elMostrarBtn = document.getElementById('mostrarBtn')
 const mostrarProductos = () => {
   customFetch(2000, productos).then(
       data => {
-          let lista = document.createElement("ul")
+          let lista = document.createElement("div")
           for (let i=0; i<data.length; i++) {
-              let p = document.createElement("li")
-              p.innerHTML = data[i].title
+              let h3 = document.createElement("h3")
+              h3.innerHTML = data[i].title
+
+              let cardDiv = document.createElement("div")
+              cardDiv.className = "card-product"
 
               let image = document.createElement("img")
               image.src = data[i].image
 
-              lista.append(p)
-              lista.append(image)
+              let price = document.createElement("p")
+              price.innerHTML = `US$ ${data[i].price}`
+
+              let buyButton = document.createElement("button")
+              buyButton.innerHTML = 'Buy'
+
+              cardDiv.append(h3)
+              cardDiv.append(image)
+              cardDiv.append(price)
+              cardDiv.append(buyButton)
+              lista.append(cardDiv)
           }
           document.getElementById("root").append(lista)
       }
@@ -37,7 +49,7 @@ const mostrarProductos = () => {
 elMostrarBtn.addEventListener('click', mostrarProductos)
 
 
-//! tarea:
+//! TAREA:
 //  crear data del producto, 10 productos
 //  crear productos como tarjetas, agregar estilos
 //  HACER UNA ESPECIE DE PROMESA PARA HACER EL ASYNC
